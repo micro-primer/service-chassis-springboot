7fa57dbf734c0027d97a4a9ac9b54c5fb6766fce
kubamarchwicki <kuba@marchwicki.pl>
2021-02-14 20:49:03 +0100

    Adding black box tests to verify configuration

--- blackboxtests/src/test/java/com/pttrn42/microprimer/servicechassispringboot/SmokeTest.java
+++ blackboxtests/src/test/java/com/pttrn42/microprimer/servicechassispringboot/SmokeTest.java
@@ -1,5 +1,6 @@
 package com.pttrn42.microprimer.servicechassispringboot;
 
+import io.restassured.http.ContentType;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 @Slf4j
 public class SmokeTest {
@@ -19,7 +22,6 @@ public class SmokeTest {
 
     @RepeatedTest(value = SystemTestEnvironment.SERVICE_NUM_INSTANCES, name = TEST_NAME_PER_SERVICE_INSTANCE)
     void pingPongs(RepetitionInfo repetitionInfo) {
-        // @formatter:off
         given()
                 .baseUri(environment.serviceUrl(repetitionInfo.getCurrentRepetition())) //example how to get url of the specific instance
         .when()
@@ -27,12 +29,10 @@ public class SmokeTest {
         .then()
                 .body(containsString("pong"))
                 .statusCode(200);
-        // @formatter:on
     }
 
     @Test
     void shouldBeHealthy() {
-        // @formatter:off
         given()
                 .baseUri(environment.serviceUrl()) //example how to get url of the random instance
         .when()
@@ -40,7 +40,42 @@ public class SmokeTest {
         .then()
                 .body("status", equalTo("UP"))
                 .statusCode(200);
-        // @formatter:on
+    }
+
+    @Test
+    void shouldExposeJmxControls() {
+        given()
+                .baseUri(environment.serviceUrl()) //example how to get url of the random instance
+                .queryParam("mimeType", "application/json")
+        .when()
+                .get("/actuator/hawtio/jolokia")
+        .then()
+                .body("status", equalTo(200))
+                .body("request.type", equalTo("version"))
+                .statusCode(200);
+    }
+
+    @Test
+    void shouldExposeThreadDump() {
+        given()
+                .baseUri(environment.serviceUrl()) //example how to get url of the random instance
+        .when()
+                .get("/actuator/threaddump")
+        .then()
+                .body("threads", hasSize(greaterThan(0)))
+                .statusCode(200);
+    }
+
+    @Test
+    void shouldExposePrometheusMetrics() {
+        given()
+                .baseUri(environment.serviceUrl()) //example how to get url of the random instance
+                .accept(ContentType.TEXT)
+        .when()
+                .get("/actuator/prometheus")
+        .then()
+                .body(containsString("app=\"service-chassis-springboot\""))
+                .statusCode(200);
     }
 
 }
